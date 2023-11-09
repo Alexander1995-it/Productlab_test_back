4013b6805a476441279596b75675be64ba216775
Alexander1995-it <sany_nax@mail.ru>
2023-11-09 21:51:07 +0300

    fix vercel.json

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { app } from "./src/app";
 
-const port = 3002;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
