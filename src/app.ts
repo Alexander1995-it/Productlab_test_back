e52a772e94d95d369e64aed8f7fab510e1a0c5c4
Alexander1995-it <sany_nax@mail.ru>
2023-11-10 21:04:38 +0300

    fix app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,52 @@
-import express from "express";
-import { getProductlabRoutes } from "./routes/productlub";
+import express, { Response } from "express";
 import { db } from "./db/db";
+import { RequestWithBody } from "./types";
+import { productlabRepository } from "./repository/courses-repository";
+
+const jwt = require("jsonwebtoken");
+
+const secretKey = "mySecretKey";
 
 export const app = express();
 export const jsonBodyMiddleware = express.json();
 
+const productlabRouter = express.Router();
+
+productlabRouter.get("/product", (req, res) => {
+  res.json({
+    name: "aaa",
+    age: "12",
+  });
+});
+
+productlabRouter.post(
+  "/login",
+  (
+    req: RequestWithBody<{ email: string; password: string }>,
+    res: Response<any>,
+  ) => {
+    let user = productlabRepository.fountLoginAndPassword(
+      req.body.email,
+      req.body.password,
+    );
+    if (user) {
+      if (user.token) {
+        res.json({ ...user, token: user.token });
+      } else {
+        let token = jwt.sign({ user }, secretKey, { expiresIn: "1h" });
+        db.users.forEach((el) => {
+          if (el.id === user?.id) {
+            el.token = token;
+          }
+        });
+        res.json({ ...user, token });
+      }
+    } else {
+      res.status(401).json({ messages: "Incorrect password or login" });
+    }
+  },
+);
+
 app.use(jsonBodyMiddleware);
 
-app.use("/", getProductlabRoutes(db));
+app.use("/", productlabRouter);
--- src/routes/productlub.ts
+++ src/routes/productlub.ts
@@ -1,49 +1,46 @@
-import { RequestWithBody } from "../types";
-import express, { Response } from "express";
 import { dbType } from "../db/db";
-import { productlabRepository } from "../repository/courses-repository";
 
 const jwt = require("jsonwebtoken");
 
 const secretKey = "mySecretKey";
 
 export const getProductlabRoutes = (db: dbType) => {
-  const productlabRouter = express.Router();
-
-  productlabRouter.get("/product", (req, res) => {
-    res.json({
-      name: "aaa",
-      age: "12",
-    });
-  });
-
-  productlabRouter.post(
-    "/login",
-    (
-      req: RequestWithBody<{ email: string; password: string }>,
-      res: Response<any>,
-    ) => {
-      let user = productlabRepository.fountLoginAndPassword(
-        req.body.email,
-        req.body.password,
-      );
-      if (user) {
-        if (user.token) {
-          res.json({ ...user, token: user.token });
-        } else {
-          let token = jwt.sign({ user }, secretKey, { expiresIn: "1h" });
-          db.users.forEach((el) => {
-            if (el.id === user?.id) {
-              el.token = token;
-            }
-          });
-          res.json({ ...user, token });
-        }
-      } else {
-        res.status(401).json({ messages: "Incorrect password or login" });
-      }
-    },
-  );
+  // const productlabRouter = express.Router();
+  //
+  // productlabRouter.get("/product", (req, res) => {
+  //   res.json({
+  //     name: "aaa",
+  //     age: "12",
+  //   });
+  // });
+  //
+  // productlabRouter.post(
+  //   "/login",
+  //   (
+  //     req: RequestWithBody<{ email: string; password: string }>,
+  //     res: Response<any>,
+  //   ) => {
+  //     let user = productlabRepository.fountLoginAndPassword(
+  //       req.body.email,
+  //       req.body.password,
+  //     );
+  //     if (user) {
+  //       if (user.token) {
+  //         res.json({ ...user, token: user.token });
+  //       } else {
+  //         let token = jwt.sign({ user }, secretKey, { expiresIn: "1h" });
+  //         db.users.forEach((el) => {
+  //           if (el.id === user?.id) {
+  //             el.token = token;
+  //           }
+  //         });
+  //         res.json({ ...user, token });
+  //       }
+  //     } else {
+  //       res.status(401).json({ messages: "Incorrect password or login" });
+  //     }
+  //   },
+  // );
 
   return productlabRouter;
 };
