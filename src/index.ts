bffd9d597993d506be1105d64137fdaf8499ebc0
Alexander1995-it <sany_nax@mail.ru>
2023-11-22 13:31:27 +0300

    fix  productlabRouter

--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/images/image1", (req, res) => {
-  const imagePath = path.join(imagesDirectory, "image1.jpg");
-  res.send(imagePath);
-  app.use(jsonBodyMiddleware);
-});
+// app.get("/images/image1", (req, res) => {
+//   const imagePath = path.join(imagesDirectory, "image1.jpg");
+//   res.send(imagePath);
+//   app.use(jsonBodyMiddleware);
+// });
 
 app.use("/", productlabRouter);
 
